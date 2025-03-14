7302848b0c436e33efa16fe4847fb7ef66358356
Alanote75 <alexandre.ledanff@ratpdev.com>
2025-03-14 16:08:33 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,14 +1,18 @@
 export default async function handler(req, res) {
-  const { url } = req.query;
-  if (!url) {
-    return res.status(400).json({ error: "URL manquante" });
+  const { product_id } = req.query;
+  const apiKey = process.env.SERPAPI_KEY;
+
+  if (!product_id || !apiKey) {
+    return res.status(400).json({ error: "Missing product_id or API key" });
   }
+
+  const url = `https://serpapi.com/search.json?engine=google_play_product&gl=fr&store=apps&product_id=${product_id}&api_key=${apiKey}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Erreur lors de la récupération des données" });
+    res.status(500).json({ error: "Erreur de récupération des données" });
   }
 }
